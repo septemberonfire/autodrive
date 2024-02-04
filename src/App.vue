89838860642767afe4c2f8d47740ce1c4a1036b6
septemberonfire <dat.a.a@yandex.ru>
2024-02-04 16:37:49 +0300

    initial commit, added deploy.yml for GH actions

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-pink-200 flex justify-center">
+    <HelloWorld />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
